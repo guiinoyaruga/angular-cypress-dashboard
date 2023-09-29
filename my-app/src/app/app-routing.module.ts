cd44b9249f0733c019504eb1729ab3f8153ae6ca
Guilherme <willfkrause@outlook.com>
2023-09-29 17:28:37 -0300

    add new component in the route module

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { RelatorioDiarioComponent } from './relatorio-diario/relatorio-diario.component';
 import { PaginaInicioComponent } from './pagina-inicio/pagina-inicio.component';
 import { RelatorioSemanalComponent } from './relatorio-semanal/relatorio-semanal.component';
+import { GraficoDiarioComponent } from './grafico-diario/grafico-diario.component';
 
 const routes: Routes = [
   { path: '', component: PaginaInicioComponent },
   { path: 'diario', component: RelatorioDiarioComponent },
-  { path: 'semanal', component: RelatorioSemanalComponent}
+  { path: 'semanal', component: RelatorioSemanalComponent},
+  { path: 'grafico', component: GraficoDiarioComponent },
 ];
 
 @NgModule({
