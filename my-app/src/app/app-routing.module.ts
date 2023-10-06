1544fb8ed9ad70313274717de0b56ca2418e4bd3
Guilherme <willfkrause@outlook.com>
2023-10-06 10:57:13 -0300

    add route mensal component

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { RelatorioDiarioComponent } from './relatorio-diario/relatorio-diario.component';
 import { PaginaInicioComponent } from './pagina-inicio/pagina-inicio.component';
 import { RelatorioSemanalComponent } from './relatorio-semanal/relatorio-semanal.component';
+import { RelatorioMensalComponent } from './relatorio-mensal/relatorio-mensal.component';
 import { GraficoDiarioComponent } from './grafico-diario/grafico-diario.component';
 
 const routes: Routes = [
   { path: '', component: PaginaInicioComponent },
   { path: 'diario', component: RelatorioDiarioComponent },
   { path: 'semanal', component: RelatorioSemanalComponent},
+  { path: 'mensal', component: RelatorioMensalComponent},
   { path: 'grafico', component: GraficoDiarioComponent },
 ];
 
